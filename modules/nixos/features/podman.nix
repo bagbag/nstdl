095a72ae805bb63a8d92a96ffd4102310aeb59da
Patrick Hein <bagbag98@googlemail.com>
2026-07-29 17:39:50 +0200

    fix(podman): support registry option variants

--- modules/nixos/features/podman.nix
+++ modules/nixos/features/podman.nix
@@ -1,11 +1,27 @@
+{ lib, options, ... }:
+let
+  registryConfiguration =
+    if lib.hasAttrByPath [ "virtualisation" "containers" "registries" "settings" ] options then
+      {
+        settings.registries.search.registries = [
+          "docker.io"
+          "quay.io"
+        ];
+      }
+    else
+      {
+        # Compatibility for nixpkgs releases that predate registries.settings.
+        search = [
+          "docker.io"
+          "quay.io"
+        ];
+      };
+in
 {
   virtualisation = {
     containers = {
       enable = true;
-      registries.search = [
-        "docker.io"
-        "quay.io"
-      ];
+      registries = registryConfiguration;
       policy = {
         default = [ { type = "reject"; } ];
         transports = {
